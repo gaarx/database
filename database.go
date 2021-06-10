b8bc2ba1c82aa3f3f07a6c2bd59100fd5d884f5f
Ivan Plotnikov <sevansd@gmail.com>
2021-06-10 17:48:49 +0700

    implement new version of gaarx

--- database.go
+++ database.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gaarx/gaarx"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"github.com/rs/zerolog/log"
 )
 
 func GetConnString(user, pass, host, port, dbName string) string {
 	return user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + dbName + "?charset=utf8mb4,utf8&parseTime=true&sql_mode=ansi"
 }
 
-// Include Database
+// WithDatabase is option for gaarx and include Database
 func WithDatabase(conn string, entities ...interface{}) gaarx.Option {
 	// TODO: get rid of mySQL
 	return func(app *gaarx.App) error {
@@ -19,14 +20,30 @@ func WithDatabase(conn string, entities ...interface{}) gaarx.Option {
 			conn,
 		)
 		if err != nil {
-			app.GetLog().Fatal(err)
+			log.Fatal().Err(err)
 			panic(err)
 		}
-		db.SetLogger(app.GetLog())
 		db.Set("gorm:table_options", "CHARSET=utf8")
 		for _, e := range entities {
 			db.AutoMigrate(e)
 		}
+		app.SetDatabase(db)
+		return nil
+	}
+}
+
+// WithDatabaseNoMigrate is option for gaarx and include Database without auto migrate entities
+func WithDatabaseNoMigrate(conn string) gaarx.Option {
+	return func(app *gaarx.App) error {
+		db, err := gorm.Open(
+			"mysql",
+			conn,
+		)
+		if err != nil {
+			log.Fatal().Err(err)
+			panic(err)
+		}
+		db.Set("gorm:table_options", "CHARSET=utf8")
 		app.SetDatabase(db)
 		return nil
 	}
